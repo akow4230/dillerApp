39a373976d5802e32fe69e587b4b153413917c1b
Ravshan8860 <127499325+Ravshan88@users.noreply.github.com>
2023-07-28 16:11:31 +0500

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <h1>Tailwind css ulandi</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Tailwind css ulandi</h1>
+        </div>
+    );
 }
 
 export default App;
