e9a9d9a9607edcf26ef759333d9d631427004a8f
Rustam Tursunov <rustamtursunov0924@gmail.com>
2023-07-29 17:15:41 +0500

    Updated

--- frontend/src/Components/utils/config/instance/index.js
+++ frontend/src/Components/utils/config/instance/index.js
@@ -1,73 +1,60 @@
 import axios from "axios";
 
-export default function myAxiosFunction(url, method, data) {
-    const token = localStorage.getItem("access_token");
-
-    const instance = axios.create({
-        baseURL: "http://localhost:8080",
+export default function (url, method, data) {
+    let token = localStorage.getItem("access_token");
+    return axios({
+        url: "http://localhost:8080" + url,
+        method: method,
+        data: data,
         headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-
-    return instance({
-        url,
-        method,
-        data,
-    })
-        .then((res) => {
-            if (res.data) {
-                return {
-                    error: false,
-                    data: res.data,
-                };
-            }
-        })
-        .catch((err) => {
-            if (err.response.status === 401) {
-                const refreshToken = localStorage.getItem("refresh_token");
-                if (!refreshToken) {
-                    console.log("User is not authenticated. Redirecting to login page...");
-                    // Handle redirection to login page here, you may use 'window.location.href = "/login";'
+            "Authorization": token
+        }
+    }).then((res) => {
+        if (res.data) {
+            return {
+                error: false,
+                data: res.data
+            };
+        }
+    }).catch((err) => {
+        if (err.response.status === 401) {
+            console.log(localStorage.getItem("refresh_token")===undefined)
+            // Returning the inner promise
+            return axios({
+                url: `http://localhost:8080/api/v1/auth/refresh?refreshToken=${localStorage.getItem("refresh_token")}`,
+                method: "POST"
+            }).then((res) => {
+                localStorage.setItem("access_token", res.data);
+                // Returning the inner promise
+                return axios({
+                    url: "http://localhost:8080" + url,
+                    method: method,
+                    data: data,
+                    headers: {
+                        "Authorization": localStorage.getItem("access_token")
+                    }
+                }).then((res) => {
+                    return {
+                        error: false,
+                        data: res.data
+                    };
+                }).catch((err) => {
                     return {
                         error: true,
-                        data: err.response.data,
+                        data: err.response.data
                     };
-                }
-
-                console.log("Refreshing access token...");
-
-                return axios
-                    .post(`http://localhost:8080/api/v1/auth/refresh?refreshToken=${refreshToken}`)
-                    .then((res) => {
-                        const newAccessToken = res.data;
-                        localStorage.setItem("access_token", newAccessToken);
-
-                        // Retry the original request with the new access token
-                        return instance({
-                            url,
-                            method,
-                            data,
-                        }).then((res) => {
-                            return {
-                                error: false,
-                                data: res.data,
-                            };
-                        });
-                    })
-                    .catch((err) => {
-                        console.log("Failed to refresh access token.");
-                        // Handle redirection to login page here, you may use 'window.location.href = "/login";'
-                        return {
-                            error: true,
-                            data: err.response.data,
-                        };
-                    });
-            } else {
+                });
+            }).catch((err) => {
                 return {
                     error: true,
-                    data: err.response.data,
+                    data: err.response.data
                 };
-            }
-        });
+            });
+        } else {
+            return {
+                error: true,
+                data: err.response.data
+            };
+        }
+    });
 }
