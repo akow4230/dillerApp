75abd0222b654dfd3866c1f73ec1529555445131
haqnazarov <haqnazarovjavohirjon@gmail.com>
2023-08-01 14:52:40 +0500

    Universal Table completed

--- frontend/src/Components/Dashboard/index.jsx
+++ frontend/src/Components/Dashboard/index.jsx
@@ -1,12 +1,12 @@
 // Index.js
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import './style.css';
 import DashboardTopBar from "./DashboardTopBar/index";
 import DashboardLeftMenu from "./DashboardLeftMenu/index";
-import { useSelector, useDispatch } from 'react-redux';
-import { navigateTo } from "../../redux/reducers/DashboardSlice";
-import {Outlet} from "react-router-dom";
+import {useDispatch, useSelector} from 'react-redux';
+import {navigateTo} from "../../redux/reducers/DashboardSlice";
 import Table from "../UniversalUI/Table/Table";
+
 // import {useNavigate} from "react-router-dom"; // Import the navigateTo action creator
 
 function Index(props) {
@@ -17,6 +17,7 @@ function Index(props) {
             key: "id",
             type: "int",
             show: true,
+            order: 1
         },
         {
             id: 2,
@@ -24,19 +25,21 @@ function Index(props) {
             key: "title",
             type: "string",
             show: true,
+            order: 2
         },
         {
             id: 3,
             title: "Url",
             key: "url",
             type: "string",
-            show: true
+            show: true,
+            order: 3
         }]
     const dispatch = useDispatch();
     const response = useSelector((state) => state.dashboard);
     // const navigate = useNavigate();
     useEffect(() => {
-        dispatch({ type: 'dashboard/getDashboardData' });
+        dispatch({type: 'dashboard/getDashboardData'});
     }, [dispatch]);
 
     useEffect(() => {
@@ -48,15 +51,15 @@ function Index(props) {
     return (
         <div className={"dashboard"}>
             <div className="top-bar">
-                <DashboardTopBar data={response?.data} />
+                <DashboardTopBar data={response?.data}/>
             </div>
             <div className="bottom-bar d-flex">
                 <div className="left-menu">
-                    <DashboardLeftMenu />
+                    <DashboardLeftMenu/>
                 </div>
-                <div className={"right-menu w-100 p-4"} style={{background:"#c2c5d5"}}>
+                <div className={"right-menu w-100 p-4"} style={{background: "#c2c5d5"}}>
                     <Table
-                        isDark={false}
+                        // isDark={true}
                         requestApi={"/api/v1/test?page={page}&size={limit}"}
                         columns={columns}
                     />
--- frontend/src/redux/reducers/TableSlice.js
+++ frontend/src/redux/reducers/TableSlice.js
@@ -3,10 +3,13 @@ import {createSlice} from "@reduxjs/toolkit";
 const initialState = {
     isLoading: false,
     error: null,
+    darkTheme: false,
     url: "",
-
+    modal: false,
     pageSize: 10,
     currentPage: 1,
+    modalColumns: [],
+    currentDragingColumn: 0,
     data: {
         data: [],
         totalElements: 0,
@@ -20,12 +23,16 @@ const tableSlice = createSlice({
     initialState,
     reducers: {
         getTableData: function (state, action) {
+            state.modalColumns = action.payload.columns;
             action.payload.url = action.payload.url.replaceAll("{page}", action.payload.page)
             action.payload.url = action.payload.url.replaceAll("{limit}", action.payload.size)
             state.url = action.payload.url
             state.data.columns = action.payload.columns
             state.currentPage = action.payload.page
             state.pageSize = action.payload.size
+            if (action.payload.isDark !== undefined) {
+                state.darkTheme = action.payload.isDark
+            }
         },
         getTableDataSuccess(state, action) {
             state.data.data = action.payload.data
@@ -48,6 +55,29 @@ const tableSlice = createSlice({
                     break;
                 }
             }
+        },
+        setCurrentDragingColumn: (state, action) => {
+            state.currentDragingColumn = action.payload;
+            console.log(state.currentDragingColumn)
+        },
+        changeOrder: (state, action) => {
+            const draggedElementIndex = state.currentDragingColumn;
+            const droppedElementIndex = action.payload;
+
+            [
+                state.modalColumns[draggedElementIndex],
+                state.modalColumns[droppedElementIndex],
+            ] = [
+                state.modalColumns[droppedElementIndex],
+                state.modalColumns[draggedElementIndex],
+            ];
+        },
+        saveColumnsOrders: (state, action) => {
+            state.data.columns = state.modalColumns;
+            state.modal = false
+        },
+        toggleModal: (state, action) => {
+            state.modal = !state.modal
         }
     }
 })
@@ -56,7 +86,11 @@ export const {
     getTableDataSuccess,
     changeTableDataSize,
     changeTableDataPage,
-    changeTableColumns
+    changeTableColumns,
+    setCurrentDragingColumn,
+    changeOrder,
+    saveColumnsOrders,
+    toggleModal
 } = tableSlice.actions
 
 export default tableSlice.reducer
