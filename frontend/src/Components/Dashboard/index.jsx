100d1978e83f78fb2bf2388d923041aa0e0cc1c3
haqnazarov <haqnazarovjavohirjon@gmail.com>
2023-08-02 11:24:48 +0500

    Table JSX update

--- frontend/src/Components/Dashboard/index.jsx
+++ frontend/src/Components/Dashboard/index.jsx
@@ -1,14 +1,12 @@
 // Index.js
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import './style.css';
 import DashboardTopBar from "./DashboardTopBar/index";
 import DashboardLeftMenu from "./DashboardLeftMenu/index";
-import { useSelector, useDispatch } from 'react-redux';
-import { navigateTo } from "../../redux/reducers/DashboardSlice";
-import {Outlet} from "react-router-dom";
+import {useDispatch, useSelector} from 'react-redux';
+import {navigateTo} from "../../redux/reducers/DashboardSlice";
 import Table from "../UniversalUI/Table/Table";
-import UModal from "../UniversalUI/Modal/UModal";
-import button from "bootstrap/js/src/button";
+
 // import {useNavigate} from "react-router-dom"; // Import the navigateTo action creator
 
 function Index(props) {
@@ -17,7 +15,7 @@ function Index(props) {
             id: 1,
             title: "Id",
             key: "id",
-            type: "int",
+            type: "jsx",
             show: true,
         },
         {
@@ -35,25 +33,25 @@ function Index(props) {
             show: true
         }]
     const elements = [{
-        name:"Name",
-        key:"name",
-        type:"text"
+        name: "Name",
+        key: "name",
+        type: "text"
     },
         {
-            name:"Age",
-            key:"age",
-            type:"number"
-        },{
-            name:"Number",
-            key:"number",
-            type:"number"
+            name: "Age",
+            key: "age",
+            type: "number"
+        }, {
+            name: "Number",
+            key: "number",
+            type: "number"
         },
     ]
     const dispatch = useDispatch();
     const response = useSelector((state) => state.dashboard);
     // const navigate = useNavigate();
     useEffect(() => {
-        dispatch({ type: 'dashboard/getDashboardData' });
+        dispatch({type: 'dashboard/getDashboardData'});
     }, [dispatch]);
 
     useEffect(() => {
@@ -65,15 +63,20 @@ function Index(props) {
     return (
         <div className={"dashboard"}>
             <div className="top-bar">
-                <DashboardTopBar data={response?.data} />
+                <DashboardTopBar data={response?.data}/>
             </div>
             <div className="bottom-bar d-flex">
                 <div className="left-menu">
-                    <DashboardLeftMenu />
+                    <DashboardLeftMenu/>
                 </div>
-                <div className={"right-menu w-100 p-4"} style={{background:"#c2c5d5"}}>
+                <div className={"right-menu w-100 p-4"} style={{background: "#c2c5d5"}}>
+                    <Table
+                        isDark={false}
+                        requestApi={"/api/v1/test?page={page}&size={limit}"}
+                        columns={columns}
+                    />
                     {/*<UModal elements={elements} title={"Modal"}/>*/}
-                    <Outlet />
+                    {/*<Outlet />*/}
                 </div>
             </div>
         </div>
